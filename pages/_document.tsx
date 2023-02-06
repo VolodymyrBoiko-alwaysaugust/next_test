3de13e636b8d642f65a7b06d565172f52ba26f9c
Ivan <ivan.bobyk@uvik.net>
2023-02-06 21:44:42 +0200

    add og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+
   return (
     <Html lang="en">
       <Head>
-        <meta property="og:title" content="The Gig Life" />
-        <meta property="og:description" content="Join The Gig Life to get the most relevant news, politics, road stories and tips & tools related to the gig economy delivered to your inbox" />
-        <meta property="og:image" content="https://my-og-img.vercel.app/api/og" />
+        
       </Head>
       <body>
         <Main />
