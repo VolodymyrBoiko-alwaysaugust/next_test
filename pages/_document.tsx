e7a8a5218df524317bd86e43cb9b8411713d69fb
Ivan <ivan.bobyk@uvik.net>
2023-02-06 21:53:06 +0200

    add og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import OgImage from '../public/og_image.png';
 
-export default function Document() {
+function Document({host}: {host: string}) {
 
   return (
     <Html lang="en">
       <Head>
-        
+        <meta property="og:title" content="The Gig Life" />
+        <meta property="og:description" content="Join The Gig Life to get the most relevant news, politics, road stories and tips & tools related to the gig economy delivered to your inbox" />
+        <meta property="og:image" content={`${host}${OgImage.src}`} />
       </Head>
       <body>
         <Main />
@@ -14,3 +17,15 @@ export default function Document() {
     </Html>
   )
 }
+
+
+Document.getInitialProps = async (ctx: any) => {
+  const { req, query, res, asPath, pathname } = ctx;
+  let host;
+  if (req) {
+    host = req.headers.host
+  }
+  return { host: String(host) }
+}
+
+export default Document
