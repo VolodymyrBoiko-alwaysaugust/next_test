5db253a014e3365b9a73b20c3c503bfe20e23de0
Ivan <ivan.bobyk@uvik.net>
2023-02-17 22:03:09 +0200

    add priority

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -61,7 +61,7 @@ const Slider = ({images}) => {
       {images && images.map((image, index) => {
             return (
               <SwiperSlide key={index}>
-                <Image src={image}/>
+                <Image src={image} priority placeholder='blur'/>
               </SwiperSlide>
             );
           }
